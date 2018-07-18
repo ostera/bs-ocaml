14966bcddece3cbacf3c5b10228f49ab70de2d55
Hongbo Zhang <bobzhang1988@gmail.com>
2018-07-18 22:48:13 +0800

    patch apply config, misc
    val setup signature changed to `setting option -> unit`
    sophisticated heuristics on whether `should_enable_color
    
    Signed-off-by: Hongbo Zhang <bobzhang1988@gmail.com>

--- utils/misc.mli
+++ utils/misc.mli
@@ -165,3 +165,52 @@ val cut_at : string -> char -> string * string
    Raise [Not_found] if the character does not appear in the string
    @since 4.01
 *)
+
+
+
+
+
+(* Color handling *)
+module Color : sig
+  type color =
+    | Black
+    | Red
+    | Green
+    | Yellow
+    | Blue
+    | Magenta
+    | Cyan
+    | White
+  ;;
+
+  type style =
+    | FG of color (* foreground *)
+    | BG of color (* background *)
+    | Bold
+    | Reset
+#if undefined BS_NO_COMPILER_PATCH then
+    | Dim
+#end
+
+  val ansi_of_style_l : style list -> string
+  (* ANSI escape sequence for the given style *)
+
+  type styles = {
+    error: style list;
+    warning: style list;
+    loc: style list;
+  }
+
+  val default_styles: styles
+  val get_styles: unit -> styles
+  val set_styles: styles -> unit
+
+  val setup : Clflags.color_setting -> unit
+  (* [setup opt] will enable or disable color handling on standard formatters
+     according to the value of color setting [opt].
+     Only the first call to this function has an effect. *)
+
+  val set_color_tag_handling : Format.formatter -> unit
+  (* adds functions to support color tags to the given formatter. *)
+end
+
